e37947f2ebc17b7210d89ca4ce5c63d1b673698c
Romain Bareau <baromain.romain.barreau31@gmail.com>
2025-03-04 16:47:15 +0100

    feat(easy) add MainArgs class making a calculator with args and user input

---
+++ src/fr/ynov/java/easy/MainArgs.java
@@ -0,0 +1,50 @@
+package fr.ynov.java.easy;
+
+import java.util.Scanner;
+
+public class MainArgs {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Please provide two numbers as arguments."); // verify that the user has provided two args
+            return;
+        }
+
+        double num1, num2;
+        try {
+            // parse the args as doubles
+            num1 = Double.parseDouble(args[0]);
+            num2 = Double.parseDouble(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format. Please provide valid numbers."); // verify that the args are numbers
+            return;
+        }
+
+        System.out.println("Enter an operator (+, -, *, /): ");
+        String operator = System.console().readLine(); // read the operator from the console
+
+        double result;
+        switch (operator) { // perform the operation based on the operator
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("Cannot divide by zero."); // verify that the user is not dividing by zero
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default: // if the operator is invalid, print an error message
+                System.out.println("Invalid operator. Please use one of +, -, *, /.");
+                return;
+        }
+
+        System.out.println("The result of the operation is: " + result);
+    }
+}
